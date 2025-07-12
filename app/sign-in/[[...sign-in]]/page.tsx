127691bacc77d4496f9bd4a9f54f663fde236186
harshit51046 <harshitkatiyar817@gmail.com>
2025-07-12 14:40:36 +0000

    provider.tsx

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import SignInForm from '@/components/SignInForm'
+import { HeroUIProvider } from '@heroui/react'
 
 const Page = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
       <main className="flex-1 flex justify-center items-center p-6">
-        <SignInForm />
+        <HeroUIProvider>
+          <SignInForm />
+        </HeroUIProvider>
       </main>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,33 @@
 // tailwind.config.js
-const {heroui} = require("@heroui/theme");
+import {heroui} from "@heroui/react";
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./node_modules/@heroui/theme/dist/components/(button|card|divider|input|ripple|spinner|form).js"
+// ],
+//   theme: {
+//     extend: {},
+//   },
+//   darkMode: "class",
+//   plugins: [heroui()],
+// };
+
+
+// tailwind.config.js
+
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
     "./node_modules/@heroui/theme/dist/components/(button|card|divider|input|ripple|spinner|form).js"
-],
+  ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [heroui()],
-};
+  plugins: [heroui()]
+}
+
+export default config;
